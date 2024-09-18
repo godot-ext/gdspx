46c674cc3b7dca08eb5466efe88eba88ca2765df
jiepengtan <jiepengtan@gmail.com>
2024-09-18 10:19:29 +0800

    Add callback gen

--- internal/webffi/callbacks.gen.go
+++ internal/webffi/callbacks.gen.go
@@ -14,209 +14,243 @@ import (
 	"syscall/js"
 )
 
-func onSpxCallbackOnEngineStart(this js.Value, args []js.Value) interface{} {
+func gdspxOnEngineStart(this js.Value, args []js.Value) interface{} {
 	println("GDExtensionSpxCallbackOnEngineStart")
 	callbacks.OnEngineStart()
 	return nil
 }
-func onSpxCallbackOnEngineUpdate(this js.Value, args []js.Value) interface{} {
-	println("GDExtensionSpxCallbackOnEngineUpdate")
+func gdspxOnEngineUpdate(this js.Value, args []js.Value) interface{} {
 	callbacks.OnEngineUpdate(0.016)
 	return nil
 }
-func onSpxCallbackOnEngineFixedUpdate(this js.Value, args []js.Value) interface{} {
-	println("GDExtensionSpxCallbackOnEngineFixedUpdate")
+func gdspxOnEngineFixedUpdate(this js.Value, args []js.Value) interface{} {
 	callbacks.OnEngineFixedUpdate(0.016)
 	return nil
 }
-func onSpxCallbackOnEngineDestroy(this js.Value, args []js.Value) interface{} {
+func gdspxOnEngineDestroy(this js.Value, args []js.Value) interface{} {
 	println("GDExtensionSpxCallbackOnEngineDestroy")
 	callbacks.OnEngineDestroy()
 	return nil
 }
-func onSpxCallbackOnSceneSpriteInstantiated(this js.Value, args []js.Value) interface{} {
+func gdspxOnSceneSpriteInstantiated(this js.Value, args []js.Value) interface{} {
 	println("GDExtensionSpxCallbackOnSceneSpriteInstantiated")
+	//callbacks.OnSceneSpriteInstantiated()
 	return nil
 }
-func onSpxCallbackOnSpriteReady(this js.Value, args []js.Value) interface{} {
+func gdspxOnSpriteReady(this js.Value, args []js.Value) interface{} {
 	println("GDExtensionSpxCallbackOnSpriteReady")
+	//callbacks.OnSpriteReady()
 	return nil
 }
-func onSpxCallbackOnSpriteUpdated(this js.Value, args []js.Value) interface{} {
+func gdspxOnSpriteUpdated(this js.Value, args []js.Value) interface{} {
 	println("GDExtensionSpxCallbackOnSpriteUpdated")
+	//callbacks.OnSpriteUpdated()
 	return nil
 }
-func onSpxCallbackOnSpriteFixedUpdated(this js.Value, args []js.Value) interface{} {
+func gdspxOnSpriteFixedUpdated(this js.Value, args []js.Value) interface{} {
 	println("GDExtensionSpxCallbackOnSpriteFixedUpdated")
+	//callbacks.OnSpriteFixedUpdated()
 	return nil
 }
-func onSpxCallbackOnSpriteDestroyed(this js.Value, args []js.Value) interface{} {
+func gdspxOnSpriteDestroyed(this js.Value, args []js.Value) interface{} {
 	println("GDExtensionSpxCallbackOnSpriteDestroyed")
+	//callbacks.OnSpriteDestroyed()
 	return nil
 }
-func onSpxCallbackOnSpriteFramesSetChanged(this js.Value, args []js.Value) interface{} {
+func gdspxOnSpriteFramesSetChanged(this js.Value, args []js.Value) interface{} {
 	println("GDExtensionSpxCallbackOnSpriteFramesSetChanged")
+	//callbacks.OnSpriteFramesSetChanged()
 	return nil
 }
-func onSpxCallbackOnSpriteAnimationChanged(this js.Value, args []js.Value) interface{} {
+func gdspxOnSpriteAnimationChanged(this js.Value, args []js.Value) interface{} {
 	println("GDExtensionSpxCallbackOnSpriteAnimationChanged")
+	//callbacks.OnSpriteAnimationChanged()
 	return nil
 }
-func onSpxCallbackOnSpriteFrameChanged(this js.Value, args []js.Value) interface{} {
+func gdspxOnSpriteFrameChanged(this js.Value, args []js.Value) interface{} {
 	println("GDExtensionSpxCallbackOnSpriteFrameChanged")
+	//callbacks.OnSpriteFrameChanged()
 	return nil
 }
-func onSpxCallbackOnSpriteAnimationLooped(this js.Value, args []js.Value) interface{} {
+func gdspxOnSpriteAnimationLooped(this js.Value, args []js.Value) interface{} {
 	println("GDExtensionSpxCallbackOnSpriteAnimationLooped")
+	//callbacks.OnSpriteAnimationLooped()
 	return nil
 }
-func onSpxCallbackOnSpriteAnimationFinished(this js.Value, args []js.Value) interface{} {
+func gdspxOnSpriteAnimationFinished(this js.Value, args []js.Value) interface{} {
 	println("GDExtensionSpxCallbackOnSpriteAnimationFinished")
+	//callbacks.OnSpriteAnimationFinished()
 	return nil
 }
-func onSpxCallbackOnSpriteVfxFinished(this js.Value, args []js.Value) interface{} {
+func gdspxOnSpriteVfxFinished(this js.Value, args []js.Value) interface{} {
 	println("GDExtensionSpxCallbackOnSpriteVfxFinished")
+	//callbacks.OnSpriteVfxFinished()
 	return nil
 }
-func onSpxCallbackOnSpriteScreenExited(this js.Value, args []js.Value) interface{} {
+func gdspxOnSpriteScreenExited(this js.Value, args []js.Value) interface{} {
 	println("GDExtensionSpxCallbackOnSpriteScreenExited")
+	//callbacks.OnSpriteScreenExited()
 	return nil
 }
-func onSpxCallbackOnSpriteScreenEntered(this js.Value, args []js.Value) interface{} {
+func gdspxOnSpriteScreenEntered(this js.Value, args []js.Value) interface{} {
 	println("GDExtensionSpxCallbackOnSpriteScreenEntered")
+	//callbacks.OnSpriteScreenEntered()
 	return nil
 }
-func onSpxCallbackOnMousePressed(this js.Value, args []js.Value) interface{} {
+func gdspxOnMousePressed(this js.Value, args []js.Value) interface{} {
 	println("GDExtensionSpxCallbackOnMousePressed")
+	//callbacks.OnMousePressed()
 	return nil
 }
-func onSpxCallbackOnMouseReleased(this js.Value, args []js.Value) interface{} {
+func gdspxOnMouseReleased(this js.Value, args []js.Value) interface{} {
 	println("GDExtensionSpxCallbackOnMouseReleased")
+	//callbacks.OnMouseReleased()
 	return nil
 }
-func onSpxCallbackOnKeyPressed(this js.Value, args []js.Value) interface{} {
+func gdspxOnKeyPressed(this js.Value, args []js.Value) interface{} {
 	println("GDExtensionSpxCallbackOnKeyPressed")
+	//callbacks.OnKeyPressed()
 	return nil
 }
-func onSpxCallbackOnKeyReleased(this js.Value, args []js.Value) interface{} {
+func gdspxOnKeyReleased(this js.Value, args []js.Value) interface{} {
 	println("GDExtensionSpxCallbackOnKeyReleased")
+	//callbacks.OnKeyReleased()
 	return nil
 }
-func onSpxCallbackOnActionPressed(this js.Value, args []js.Value) interface{} {
+func gdspxOnActionPressed(this js.Value, args []js.Value) interface{} {
 	println("GDExtensionSpxCallbackOnActionPressed")
+	//callbacks.OnActionPressed()
 	return nil
 }
-func onSpxCallbackOnActionJustPressed(this js.Value, args []js.Value) interface{} {
+func gdspxOnActionJustPressed(this js.Value, args []js.Value) interface{} {
 	println("GDExtensionSpxCallbackOnActionJustPressed")
+	//callbacks.OnActionJustPressed()
 	return nil
 }
-func onSpxCallbackOnActionJustReleased(this js.Value, args []js.Value) interface{} {
+func gdspxOnActionJustReleased(this js.Value, args []js.Value) interface{} {
 	println("GDExtensionSpxCallbackOnActionJustReleased")
+	//callbacks.OnActionJustReleased()
 	return nil
 }
-func onSpxCallbackOnAxisChanged(this js.Value, args []js.Value) interface{} {
+func gdspxOnAxisChanged(this js.Value, args []js.Value) interface{} {
 	println("GDExtensionSpxCallbackOnAxisChanged")
+	//callbacks.OnAxisChanged()
 	return nil
 }
-func onSpxCallbackOnCollisionEnter(this js.Value, args []js.Value) interface{} {
+func gdspxOnCollisionEnter(this js.Value, args []js.Value) interface{} {
 	println("GDExtensionSpxCallbackOnCollisionEnter")
+	//callbacks.OnCollisionEnter()
 	return nil
 }
-func onSpxCallbackOnCollisionStay(this js.Value, args []js.Value) interface{} {
+func gdspxOnCollisionStay(this js.Value, args []js.Value) interface{} {
 	println("GDExtensionSpxCallbackOnCollisionStay")
+	//callbacks.OnCollisionStay()
 	return nil
 }
-func onSpxCallbackOnCollisionExit(this js.Value, args []js.Value) interface{} {
+func gdspxOnCollisionExit(this js.Value, args []js.Value) interface{} {
 	println("GDExtensionSpxCallbackOnCollisionExit")
+	//callbacks.OnCollisionExit()
 	return nil
 }
-func onSpxCallbackOnTriggerEnter(this js.Value, args []js.Value) interface{} {
+func gdspxOnTriggerEnter(this js.Value, args []js.Value) interface{} {
 	println("GDExtensionSpxCallbackOnTriggerEnter")
+	//callbacks.OnTriggerEnter()
 	return nil
 }
-func onSpxCallbackOnTriggerStay(this js.Value, args []js.Value) interface{} {
+func gdspxOnTriggerStay(this js.Value, args []js.Value) interface{} {
 	println("GDExtensionSpxCallbackOnTriggerStay")
+	//callbacks.OnTriggerStay()
 	return nil
 }
-func onSpxCallbackOnTriggerExit(this js.Value, args []js.Value) interface{} {
+func gdspxOnTriggerExit(this js.Value, args []js.Value) interface{} {
 	println("GDExtensionSpxCallbackOnTriggerExit")
+	//callbacks.OnTriggerExit()
 	return nil
 }
-func onSpxCallbackOnUiReady(this js.Value, args []js.Value) interface{} {
+func gdspxOnUiReady(this js.Value, args []js.Value) interface{} {
 	println("GDExtensionSpxCallbackOnUiReady")
+	//callbacks.OnUiReady()
 	return nil
 }
-func onSpxCallbackOnUiUpdated(this js.Value, args []js.Value) interface{} {
+func gdspxOnUiUpdated(this js.Value, args []js.Value) interface{} {
 	println("GDExtensionSpxCallbackOnUiUpdated")
+	//callbacks.OnUiUpdated()
 	return nil
 }
-func onSpxCallbackOnUiDestroyed(this js.Value, args []js.Value) interface{} {
+func gdspxOnUiDestroyed(this js.Value, args []js.Value) interface{} {
 	println("GDExtensionSpxCallbackOnUiDestroyed")
+	//callbacks.OnUiDestroyed()
 	return nil
 }
-func onSpxCallbackOnUiPressed(this js.Value, args []js.Value) interface{} {
+func gdspxOnUiPressed(this js.Value, args []js.Value) interface{} {
 	println("GDExtensionSpxCallbackOnUiPressed")
+	//callbacks.OnUiPressed()
 	return nil
 }
-func onSpxCallbackOnUiReleased(this js.Value, args []js.Value) interface{} {
+func gdspxOnUiReleased(this js.Value, args []js.Value) interface{} {
 	println("GDExtensionSpxCallbackOnUiReleased")
+	//callbacks.OnUiReleased()
 	return nil
 }
-func onSpxCallbackOnUiHovered(this js.Value, args []js.Value) interface{} {
+func gdspxOnUiHovered(this js.Value, args []js.Value) interface{} {
 	println("GDExtensionSpxCallbackOnUiHovered")
+	//callbacks.OnUiHovered()
 	return nil
 }
-func onSpxCallbackOnUiClicked(this js.Value, args []js.Value) interface{} {
+func gdspxOnUiClicked(this js.Value, args []js.Value) interface{} {
 	println("GDExtensionSpxCallbackOnUiClicked")
+	//callbacks.OnUiClicked()
 	return nil
 }
-func onSpxCallbackOnUiToggle(this js.Value, args []js.Value) interface{} {
+func gdspxOnUiToggle(this js.Value, args []js.Value) interface{} {
 	println("GDExtensionSpxCallbackOnUiToggle")
+	//callbacks.OnUiToggle()
 	return nil
 }
-func onSpxCallbackOnUiTextChanged(this js.Value, args []js.Value) interface{} {
+func gdspxOnUiTextChanged(this js.Value, args []js.Value) interface{} {
 	println("GDExtensionSpxCallbackOnUiTextChanged")
+	//callbacks.OnUiTextChanged()
 	return nil
 }
 func resiterFuncPtr2Js() {
-	js.Global().Set("on_spx_callback_on_engine_start", js.FuncOf(onSpxCallbackOnEngineStart))
-	js.Global().Set("on_spx_callback_on_engine_update", js.FuncOf(onSpxCallbackOnEngineUpdate))
-	js.Global().Set("on_spx_callback_on_engine_fixed_update", js.FuncOf(onSpxCallbackOnEngineFixedUpdate))
-	js.Global().Set("on_spx_callback_on_engine_destroy", js.FuncOf(onSpxCallbackOnEngineDestroy))
-	js.Global().Set("on_spx_callback_on_scene_sprite_instantiated", js.FuncOf(onSpxCallbackOnSceneSpriteInstantiated))
-	js.Global().Set("on_spx_callback_on_sprite_ready", js.FuncOf(onSpxCallbackOnSpriteReady))
-	js.Global().Set("on_spx_callback_on_sprite_updated", js.FuncOf(onSpxCallbackOnSpriteUpdated))
-	js.Global().Set("on_spx_callback_on_sprite_fixed_updated", js.FuncOf(onSpxCallbackOnSpriteFixedUpdated))
-	js.Global().Set("on_spx_callback_on_sprite_destroyed", js.FuncOf(onSpxCallbackOnSpriteDestroyed))
-	js.Global().Set("on_spx_callback_on_sprite_frames_set_changed", js.FuncOf(onSpxCallbackOnSpriteFramesSetChanged))
-	js.Global().Set("on_spx_callback_on_sprite_animation_changed", js.FuncOf(onSpxCallbackOnSpriteAnimationChanged))
-	js.Global().Set("on_spx_callback_on_sprite_frame_changed", js.FuncOf(onSpxCallbackOnSpriteFrameChanged))
-	js.Global().Set("on_spx_callback_on_sprite_animation_looped", js.FuncOf(onSpxCallbackOnSpriteAnimationLooped))
-	js.Global().Set("on_spx_callback_on_sprite_animation_finished", js.FuncOf(onSpxCallbackOnSpriteAnimationFinished))
-	js.Global().Set("on_spx_callback_on_sprite_vfx_finished", js.FuncOf(onSpxCallbackOnSpriteVfxFinished))
-	js.Global().Set("on_spx_callback_on_sprite_screen_exited", js.FuncOf(onSpxCallbackOnSpriteScreenExited))
-	js.Global().Set("on_spx_callback_on_sprite_screen_entered", js.FuncOf(onSpxCallbackOnSpriteScreenEntered))
-	js.Global().Set("on_spx_callback_on_mouse_pressed", js.FuncOf(onSpxCallbackOnMousePressed))
-	js.Global().Set("on_spx_callback_on_mouse_released", js.FuncOf(onSpxCallbackOnMouseReleased))
-	js.Global().Set("on_spx_callback_on_key_pressed", js.FuncOf(onSpxCallbackOnKeyPressed))
-	js.Global().Set("on_spx_callback_on_key_released", js.FuncOf(onSpxCallbackOnKeyReleased))
-	js.Global().Set("on_spx_callback_on_action_pressed", js.FuncOf(onSpxCallbackOnActionPressed))
-	js.Global().Set("on_spx_callback_on_action_just_pressed", js.FuncOf(onSpxCallbackOnActionJustPressed))
-	js.Global().Set("on_spx_callback_on_action_just_released", js.FuncOf(onSpxCallbackOnActionJustReleased))
-	js.Global().Set("on_spx_callback_on_axis_changed", js.FuncOf(onSpxCallbackOnAxisChanged))
-	js.Global().Set("on_spx_callback_on_collision_enter", js.FuncOf(onSpxCallbackOnCollisionEnter))
-	js.Global().Set("on_spx_callback_on_collision_stay", js.FuncOf(onSpxCallbackOnCollisionStay))
-	js.Global().Set("on_spx_callback_on_collision_exit", js.FuncOf(onSpxCallbackOnCollisionExit))
-	js.Global().Set("on_spx_callback_on_trigger_enter", js.FuncOf(onSpxCallbackOnTriggerEnter))
-	js.Global().Set("on_spx_callback_on_trigger_stay", js.FuncOf(onSpxCallbackOnTriggerStay))
-	js.Global().Set("on_spx_callback_on_trigger_exit", js.FuncOf(onSpxCallbackOnTriggerExit))
-	js.Global().Set("on_spx_callback_on_ui_ready", js.FuncOf(onSpxCallbackOnUiReady))
-	js.Global().Set("on_spx_callback_on_ui_updated", js.FuncOf(onSpxCallbackOnUiUpdated))
-	js.Global().Set("on_spx_callback_on_ui_destroyed", js.FuncOf(onSpxCallbackOnUiDestroyed))
-	js.Global().Set("on_spx_callback_on_ui_pressed", js.FuncOf(onSpxCallbackOnUiPressed))
-	js.Global().Set("on_spx_callback_on_ui_released", js.FuncOf(onSpxCallbackOnUiReleased))
-	js.Global().Set("on_spx_callback_on_ui_hovered", js.FuncOf(onSpxCallbackOnUiHovered))
-	js.Global().Set("on_spx_callback_on_ui_clicked", js.FuncOf(onSpxCallbackOnUiClicked))
-	js.Global().Set("on_spx_callback_on_ui_toggle", js.FuncOf(onSpxCallbackOnUiToggle))
-	js.Global().Set("on_spx_callback_on_ui_text_changed", js.FuncOf(onSpxCallbackOnUiTextChanged))
+	js.Global().Set("gdspx_on_engine_start", js.FuncOf(gdspxOnEngineStart))
+	js.Global().Set("gdspx_on_engine_update", js.FuncOf(gdspxOnEngineUpdate))
+	js.Global().Set("gdspx_on_engine_fixed_update", js.FuncOf(gdspxOnEngineFixedUpdate))
+	js.Global().Set("gdspx_on_engine_destroy", js.FuncOf(gdspxOnEngineDestroy))
+	js.Global().Set("gdspx_on_scene_sprite_instantiated", js.FuncOf(gdspxOnSceneSpriteInstantiated))
+	js.Global().Set("gdspx_on_sprite_ready", js.FuncOf(gdspxOnSpriteReady))
+	js.Global().Set("gdspx_on_sprite_updated", js.FuncOf(gdspxOnSpriteUpdated))
+	js.Global().Set("gdspx_on_sprite_fixed_updated", js.FuncOf(gdspxOnSpriteFixedUpdated))
+	js.Global().Set("gdspx_on_sprite_destroyed", js.FuncOf(gdspxOnSpriteDestroyed))
+	js.Global().Set("gdspx_on_sprite_frames_set_changed", js.FuncOf(gdspxOnSpriteFramesSetChanged))
+	js.Global().Set("gdspx_on_sprite_animation_changed", js.FuncOf(gdspxOnSpriteAnimationChanged))
+	js.Global().Set("gdspx_on_sprite_frame_changed", js.FuncOf(gdspxOnSpriteFrameChanged))
+	js.Global().Set("gdspx_on_sprite_animation_looped", js.FuncOf(gdspxOnSpriteAnimationLooped))
+	js.Global().Set("gdspx_on_sprite_animation_finished", js.FuncOf(gdspxOnSpriteAnimationFinished))
+	js.Global().Set("gdspx_on_sprite_vfx_finished", js.FuncOf(gdspxOnSpriteVfxFinished))
+	js.Global().Set("gdspx_on_sprite_screen_exited", js.FuncOf(gdspxOnSpriteScreenExited))
+	js.Global().Set("gdspx_on_sprite_screen_entered", js.FuncOf(gdspxOnSpriteScreenEntered))
+	js.Global().Set("gdspx_on_mouse_pressed", js.FuncOf(gdspxOnMousePressed))
+	js.Global().Set("gdspx_on_mouse_released", js.FuncOf(gdspxOnMouseReleased))
+	js.Global().Set("gdspx_on_key_pressed", js.FuncOf(gdspxOnKeyPressed))
+	js.Global().Set("gdspx_on_key_released", js.FuncOf(gdspxOnKeyReleased))
+	js.Global().Set("gdspx_on_action_pressed", js.FuncOf(gdspxOnActionPressed))
+	js.Global().Set("gdspx_on_action_just_pressed", js.FuncOf(gdspxOnActionJustPressed))
+	js.Global().Set("gdspx_on_action_just_released", js.FuncOf(gdspxOnActionJustReleased))
+	js.Global().Set("gdspx_on_axis_changed", js.FuncOf(gdspxOnAxisChanged))
+	js.Global().Set("gdspx_on_collision_enter", js.FuncOf(gdspxOnCollisionEnter))
+	js.Global().Set("gdspx_on_collision_stay", js.FuncOf(gdspxOnCollisionStay))
+	js.Global().Set("gdspx_on_collision_exit", js.FuncOf(gdspxOnCollisionExit))
+	js.Global().Set("gdspx_on_trigger_enter", js.FuncOf(gdspxOnTriggerEnter))
+	js.Global().Set("gdspx_on_trigger_stay", js.FuncOf(gdspxOnTriggerStay))
+	js.Global().Set("gdspx_on_trigger_exit", js.FuncOf(gdspxOnTriggerExit))
+	js.Global().Set("gdspx_on_ui_ready", js.FuncOf(gdspxOnUiReady))
+	js.Global().Set("gdspx_on_ui_updated", js.FuncOf(gdspxOnUiUpdated))
+	js.Global().Set("gdspx_on_ui_destroyed", js.FuncOf(gdspxOnUiDestroyed))
+	js.Global().Set("gdspx_on_ui_pressed", js.FuncOf(gdspxOnUiPressed))
+	js.Global().Set("gdspx_on_ui_released", js.FuncOf(gdspxOnUiReleased))
+	js.Global().Set("gdspx_on_ui_hovered", js.FuncOf(gdspxOnUiHovered))
+	js.Global().Set("gdspx_on_ui_clicked", js.FuncOf(gdspxOnUiClicked))
+	js.Global().Set("gdspx_on_ui_toggle", js.FuncOf(gdspxOnUiToggle))
+	js.Global().Set("gdspx_on_ui_text_changed", js.FuncOf(gdspxOnUiTextChanged))
 }
